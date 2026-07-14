9dec53067d7a1768df7e5cae5015dad6becd7bde
Negesh Bala M <negeshbalam@gmail.com>
2026-07-14 19:11:53 +0530

    Google Login Setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 import { motion } from "framer-motion";
 import API from "../services/api";
 import logo from "../assets/eec-logo.png";
@@ -58,6 +60,61 @@ localStorage.setItem(
 
     }
   };
+  const googleLogin = async (credentialResponse) => {
+  try {
+
+    const userInfo = jwtDecode(
+      credentialResponse.credential
+    );
+
+    const email = userInfo.email;
+    const name = userInfo.name;
+
+    if (
+      !email.endsWith(
+        "@eec.srmrmp.edu.in"
+      )
+    ) {
+      alert(
+        "Only college email accounts are allowed"
+      );
+      return;
+    }
+
+    const response =
+      await API.post(
+        "/api/auth/google-login",
+        {
+          email,
+          name,
+        }
+      );
+
+    const user = response.data;
+
+    localStorage.setItem(
+      "user",
+      JSON.stringify(user)
+    );
+
+    if (user.role === "STUDENT") {
+      navigate("/student");
+    }
+    else if (user.role === "FACULTY") {
+      navigate("/faculty");
+    }
+    else if (user.role === "PRINCIPAL") {
+      navigate("/principal");
+    }
+
+  } catch (error) {
+    console.error(error);
+
+    alert(
+      "Google Login Failed"
+    );
+  }
+};
 
   return (
     <div className="login-page">
@@ -138,9 +195,20 @@ localStorage.setItem(
             Login
           </button>
 
-          <button className="google-btn">
-            Continue with Google
-          </button>
+          <div
+  style={{
+    marginTop: "15px",
+    display: "flex",
+    justifyContent: "center",
+  }}
+>
+  <GoogleLogin
+    onSuccess={googleLogin} 
+    onError={() =>
+      alert("Google Login Failed")
+    }
+  />
+</div>
 
           <p className="footer-text">
             CampusCare AI © 2026
